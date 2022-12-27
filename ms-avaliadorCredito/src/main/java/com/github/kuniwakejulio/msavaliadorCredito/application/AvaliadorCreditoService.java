a5fd643c01117386f458e831949c583c6e8a98d1
julio-kuniwake <julio.jtkuniwake@gmail.com>
2022-12-26 23:43:12 -0300

    Implementação MS avaliadorCredito buscando dados do cartao usando OpenFeign

--- ms-avaliadorCredito/src/main/java/com/github/kuniwakejulio/msavaliadorCredito/application/AvaliadorCreditoService.java
+++ ms-avaliadorCredito/src/main/java/com/github/kuniwakejulio/msavaliadorCredito/application/AvaliadorCreditoService.java
@@ -1,12 +1,16 @@
 package com.github.kuniwakejulio.msavaliadorCredito.application;
 
+import com.github.kuniwakejulio.msavaliadorCredito.application.dto.CartaoClienteDto;
 import com.github.kuniwakejulio.msavaliadorCredito.application.dto.DadosClienteDto;
 import com.github.kuniwakejulio.msavaliadorCredito.domain.model.SituacaoCliente;
+import com.github.kuniwakejulio.msavaliadorCredito.infra.feing.CartoesResourceFeign;
 import com.github.kuniwakejulio.msavaliadorCredito.infra.feing.ClienteResourceFeign;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -15,12 +19,16 @@ public class AvaliadorCreditoService {
     @Autowired
     private final ClienteResourceFeign clienteResourceFeign;
 
+    @Autowired
+    private final CartoesResourceFeign cartoesResourceFeign;
+
     public SituacaoCliente obterSituacaoCliente(String cpf) {
-        // Obter cartoes do cliente MS Cartoes
-        // Obter dados do cliente MS Cliente
-        ResponseEntity<DadosClienteDto> dadosClienteDto = clienteResourceFeign.getDadosCliente(cpf);
+
+        ResponseEntity<DadosClienteDto> dadosClienteDto = clienteResourceFeign.getDadosCliente(cpf); // Obter dados do cliente MS Cliente
+        ResponseEntity<List<CartaoClienteDto>> listdadosCartaocliente = cartoesResourceFeign.getCartoesByClienteCpf(cpf); // Obter cartoes do cliente MS Cartoes
         return SituacaoCliente.builder()
                 .dadosCliente(dadosClienteDto.getBody())
+                .cartaoClientes(listdadosCartaocliente.getBody())
                 .build();
 
     }
--- ms-avaliadorCredito/src/main/java/com/github/kuniwakejulio/msavaliadorCredito/domain/model/SituacaoCliente.java
+++ ms-avaliadorCredito/src/main/java/com/github/kuniwakejulio/msavaliadorCredito/domain/model/SituacaoCliente.java
@@ -12,7 +12,6 @@ import java.util.List;
 @Builder
 @NoArgsConstructor
 public class SituacaoCliente {
-
     private DadosClienteDto dadosCliente;
     private List<CartaoClienteDto> cartaoClientes;
 
